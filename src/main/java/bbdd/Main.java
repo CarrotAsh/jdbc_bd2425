c107cde057e4a15b59ac54db39776268bd8c2967
Samuel <samuelpremiun11@gmail.com>
2024-12-10 16:45:49 +0100

    test

--- src/main/java/bbdd/Main.java
+++ src/main/java/bbdd/Main.java
@@ -25,16 +25,47 @@ public class Main {
     
         // @TODO Prueba sus funciones
         // 1. Añade los planetas a la base de datos
+        nuevoPlaneta("Kepler-186f", 3.3e24, 8800, "Copernico");
+        nuevoPlaneta("HD 209458 b", 1.4e27, 100000, "Beta Pictoris");
+        nuevoPlaneta("LHS 1140 b", 8.3e24, 8800, "Copernico");
         // 2. Muestra por pantalla la lista de pasajeros de la cabina A-60-S
         // 3. Muestra por pantalla una lista de sistemas, planetas y número de pasajeros con origen en ellos
         // ASH es muy casina
         // Pero mucho
+
+
         
         conn.close();
     }
 
-    private static void nuevoPlaneta (String nombre, double masa, int radio, String sistema) throws SQLException {
+    private static void nuevoPlaneta (String nombre, Double masa, Integer radio, String sistema) throws SQLException {
         // @TODO Añade planetas a la base de datos
+        try{
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO planetas (nombre, masa, radio, sistema) VALUES (?,?,?,?)");
+            stmt.setString(1, nombre);
+            if (masa == null) {
+                stmt.setNull(2, java.sql.Types.DECIMAL); // Si masa es nulo, insertar NULL (si no se hace asi salta nullPointerException)
+            } else {
+                stmt.setDouble(2, masa); // Si no, insertar el valor de masa
+            }
+            if (radio == null) {
+                stmt.setNull(3, java.sql.Types.DECIMAL);  // lo mismo que pasaba con la masa
+            } else {
+                stmt.setInt(3, radio);  //
+            }
+            stmt.setString(4,sistema);
+            stmt.executeUpdate();
+            System.out.println("Creado el planeta \""+ nombre + "\"");
+        } catch(SQLException ex){
+            if (ex.getMessage().contains("Duplicate entry"))
+                System.out.println("Ya existe el planeta en la BBDD. No es necesario crearlo");
+            else
+            {
+                System.out.println(ex);
+                System.exit(1);
+            }
+        }
+
 
     }
 
